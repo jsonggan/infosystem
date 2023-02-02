634094123330daaa66c5ad205c74e07ec1a6816f
chin song <songganjs@gmail.com>
2023-02-03 02:13:40 +0800

    first update

---
+++ lib/src/main/java/week1/Fibonacci.java
@@ -0,0 +1,19 @@
+package week1;
+
+public class Fibonacci {
+    public static void main(int[] args){
+
+
+    }
+    private static String recursion(int n){
+        int i=0;
+        String result= "";
+        for(i=0; i<n ; i++){
+            if (i ==0){
+                result += i;
+            }
+        }
+        return "fsaf";
+    }
+}
+
